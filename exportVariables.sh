419486dfb1e0f0378ea7ee625e714d1f3dd787ef
Rafael Osorio <rosorio@br.ibm.com>
2021-05-03 18:22:14 -0300

    Script v0.1

---
+++ exportVariables.sh
@@ -0,0 +1,17 @@
+set -e
+
+clear
+
+source 00-variables
+
+export cp4ibasepath="${BASEPATH}"
+echo $cp4ibasepath
+
+export cp4iuser="${USERNAME}"
+echo $cp4iuser
+
+export cp4ipw="${PASSWORD}"
+echo $cp4ipw
+
+export cp4iclientid="${CLIENTID}"
+echo $cp4iclientid
